ccc43ab07a0a6c80d18c53e320aa2c6d1fef44dd
Andre Besner <besner.andre@gmail.com>
2023-06-02 20:21:54 -0400

    finish all product category routes, test confirmed w insomnia

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -39,7 +39,7 @@ router.post("/", async (req, res) => {
     const categoriesData = await Category.create(req.body);
     res.status(200).json(categoriesData);
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 });
 
@@ -54,11 +54,28 @@ router.put("/:id", async (req, res) => {
     await categoriesData.update(req.body);
     res.status(200).json(categoriesData);
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 });
 
 // delete a category by its `id` value
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", async (req, res) => {
+  try {
+    const categoriesData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if(!categoriesData){
+      res.status(404).json({ message: "No category found with that id!" });
+      return;
+    }
+
+    res.status(200).json(categoriesData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
