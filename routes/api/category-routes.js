c8564ff9d45c9ca1ea3e0ba57baf81a1c3135b72
Andre Besner <besner.andre@gmail.com>
2023-06-04 12:53:31 -0400

    fix issue where when wrong category or product id was entered a 200 status as bad JSON was returned to request

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -23,11 +23,11 @@ router.get("/:id", async (req, res) => {
     const categoriesData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }],
     });
-    res.status(200).json(categoriesData);
     if (!categoriesData) {
       res.status(404).json({ message: "No category found with that id!" });
       return;
     }
+    res.status(200).json(categoriesData);
   } catch (err) {
     res.status(500).json(err);
   }
