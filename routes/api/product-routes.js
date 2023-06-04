66bc48218b6d2afc937f5af73237bc1b5df791f9
Andre Besner <besner.andre@gmail.com>
2023-06-04 13:01:12 -0400

    add delete product by id route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -115,8 +115,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productsData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productsData) {
+      res.status(404).json({ message: "No category found with that id!" });
+      return;
+    }
+
+    res.status(200).json(productsData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
